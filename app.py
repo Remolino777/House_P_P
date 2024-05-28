418b9df0de97d0254fedeb4b39569218fa8ce369
Javier <7.matute@gmail.com>
2024-05-28 17:20:29 -0500

    app 90%

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import numpy as np
 from sklearn.tree import DecisionTreeRegressor
 
 #_____________________________Variables
+predict = 0
 
-regression = joblib.load('price_model.pkl')
+model = joblib.load('price_model.pkl')  
 
 bedrooms =  1
 bathrooms = 1
@@ -20,11 +21,58 @@ prefarea=   0
 furnishingstatus=   0
 area=   4753.029385188196
 
-v_list = [bedrooms,bathrooms, stories, mainroad, guestroom, basement, hotwaterheating,
-    airconditioning, parking, prefarea, furnishingstatus, area
-]
+
 
 img_path = r'img\Casa-01.png'
+
+#_____________________________________________SIDEBAR
+with st.sidebar:
+    with st.form('Variables', clear_on_submit=True):
+        bedrooms =  st.selectbox("How many bedrooms?",
+        ("1", "2", "3", "4"))
+        bathrooms =  st.selectbox("How many bathrooms?",
+        ("1", "2", "3",))
+        stories =  st.selectbox("How many stories?",
+        ("1", "2", "3", "4"))
+        parking =  st.selectbox("How many parking spaces?",
+        ("0","1", "2"))
+        mainroad = st.checkbox("Mainroad")    
+        guestroom = st.checkbox("Guestroom")    
+        basement=   st.checkbox("Basement")
+        hotwaterheating= st.checkbox("Hotwater heating")
+        airconditioning= st.checkbox("Airconditioning")
+        prefarea = st.checkbox("Preferred area")
+        furnishingstatus = st.checkbox("Furnished")
+        
+        #Logaritmico        
+        area = st.slider("Select house area(square foot):", 1650,16200,1650)
+        "---"        
+        submited = st.form_submit_button("Predict")
+        
+        if submited:
+            # Convertir valores de checkboxes a enteros
+            mainroad = int(mainroad)
+            guestroom = int(guestroom)
+            basement = int(basement)
+            hotwaterheating = int(hotwaterheating)
+            airconditioning = int(airconditioning)
+            prefarea = int(prefarea)
+            furnishingstatus = int(furnishingstatus)
+            parking = int(parking)
+            area = np.log(area)
+            # Crear lista de valores para la predicción
+            v_list = [int(bedrooms), int(bathrooms), int(stories), mainroad, guestroom, basement,
+                      hotwaterheating, airconditioning, int(parking), prefarea, furnishingstatus, area]
+
+            # Realizar la predicción
+            w_pred = model.predict([v_list])
+            predict = np.exp(w_pred)
+            predict = int(predict)
+
+            # Mostrar los resultados de la predicción
+            st.session_state['predict'] = predict
+
+
 
 
 #________________________________App
@@ -34,29 +82,17 @@ cl1, cl2, cl3 = st.columns([1,2,1])
 with cl2:
     "---"
     st.image(img_path)
-    "---"
-    st.subheader('Price prediction')
-    
+    "---"    
+    if 'predict' in st.session_state:
+        st.subheader('House specifications')
+        st.write(f'Bedrooms:    {bedrooms}')
+        st.write(f'Bathrooms:   {bathrooms}')
+        st.write(f'Stories:     {stories}')
+        st.write(f'House area:   {round(np.exp(area),2)} sqf')
+        st.subheader(f'House value prediction:')
+        st.title(f'{st.session_state["predict"]}.00 $')
 
 
-#_____________________________________________SIDEBAR
-with st.sidebar:
-    with st.form('Variables'):
-        bedrooms =  st.selectbox("How many bedrooms?",
-        ("1", "2", "3", "4"))
-        bathrooms =  st.selectbox("How many bathrooms?",
-        ("1", "2", "3",))
-        stories =  st.selectbox("How many stories?",
-        ("1", "2", "3", "4"))
-        mainroad = st.checkbox("Mainroad")    
-        guestroom = st.checkbox("Guestroom")    
-        basement=   st.checkbox("Basement")
-        hotwaterheating= st.checkbox("Hotwater heating")
-        airconditioning= st.checkbox("Airconditioning")
-        prefarea = st.checkbox("Preferred area")
-        furnishingstatus = st.checkbox("Furnished")
-        area = st.slider("Select house area(square foot):", 0,16200,0)
-        "---"
-        submited = st.form_submit_button("Submit")
+
 
 
